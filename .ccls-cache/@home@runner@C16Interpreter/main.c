0786d35ac6cb62ab949909bb4b1055cd2afb7b15
Iain Taylor <iain.taylor.94@gmail.com>
2023-10-21 14:08:44 +0000

    completed file I/O

--- .ccls-cache/@home@runner@C16Interpreter/main.c
+++ .ccls-cache/@home@runner@C16Interpreter/main.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/errno.h>
 
-int main(void) {
-  printf("Hello World\n");
+// Declare Functions
+void usage (char *);
+
+int main(int argc, char *argv[]) {
+
+  // File Initialization
+  FILE *gInterpreterFile;
+  
+  if (argc != 2) usage (argv[0]);
+
+  if (NULL == (gInterpreterFile = fopen(argv[1], "r"))) {
+    fprintf (stderr, "inputFile: %s: %s\n", argv[1], strerror(errno));
+    exit (EXIT_FAILURE);
+  }
+  else {
+    fprintf (stderr, "%s opened for reading.\n" , argv[1]);
+  }
+
+
+
+
+
+  
+  // File Close
+  fclose (gInterpreterFile);
+  
   return 0;
+}
+
+
+// Define Functions
+void usage (char *cmd) {
+  fprintf (stderr, "usage: %s inputFileName\n", cmd);
+  exit (EXIT_SUCCESS);
 }
